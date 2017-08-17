e3a50d0758defb14d9017bff0c42bf7806aea207
Oleg McNamara <omcnamar@students.kennesaw.edu>
2017-08-17 02:21:26 -0400

    Test Commit2

--- app/src/main/java/com/olegsagenadatrytwo/w3d3_makingrestcallsgithubprofile/MainActivity.java
+++ app/src/main/java/com/olegsagenadatrytwo/w3d3_makingrestcallsgithubprofile/MainActivity.java
@@ -22,6 +22,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+//test comment
 public class MainActivity extends AppCompatActivity {
 
     public static String URL = "https://api.github.com/users/";
